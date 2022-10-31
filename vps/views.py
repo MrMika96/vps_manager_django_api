6b0c4d8fb84480aca10a48fbaa21e541249909b8
MrMika96 <mikamc1996@gmail.com>
2022-10-31 14:59:55 +0300

    *BETA* added new values to GET vps/

--- vps/views.py
+++ vps/views.py
@@ -1,8 +1,12 @@
-from django.db.models import F, Sum, Case, When, Value, FloatField, ExpressionWrapper
+from collections import OrderedDict
+
+from django.db.models import F, Sum, Case, When, Value, FloatField, ExpressionWrapper, Count, Q
 from django_filters import rest_framework as filters
 from rest_framework import viewsets
 from rest_framework.generics import UpdateAPIView
+from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from vps.filters import VpsFilter
 from vps.models import Vps
@@ -16,6 +20,7 @@ class VpsViewSet(viewsets.ModelViewSet):
     queryset = Vps.objects.all()
     permission_classes = [IsAuthenticated]
     serializer_class = VpsSerializer
+    pagination_class = LimitOffsetPagination
     filter_backends = (filters.DjangoFilterBackend,)
     filterset_class = VpsFilter
 
@@ -40,6 +45,41 @@ class VpsViewSet(viewsets.ModelViewSet):
         if self.request.method == "GET" and self.kwargs.get("pk"):
             return VpsSingleSerializer
         return self.serializer_class
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(queryset, many=True)
+            return self.get_paginated_response(serializer.data)
+        else:
+            status_count = Vps.objects.all().aggregate(
+                started=Count("status", filter=Q(status="started")),
+                blocked=Count("status", filter=Q(status="blocked")),
+                stopped=Count("status", filter=Q(status="stopped"))
+            )
+            serializer = self.get_serializer(queryset, many=True)
+            status_count["results"] = serializer.data
+            return Response(status_count)
+
+    def get_paginated_response(self, data):
+        status_count = Vps.objects.all().aggregate(
+            started=Count("status", filter=Q(status="started")),
+            blocked=Count("status", filter=Q(status="blocked")),
+            stopped=Count("status", filter=Q(status="stopped"))
+        )
+        pagination = self.pagination_class()
+        page = self.paginate_queryset(data)
+        return Response(OrderedDict([
+            ('count', pagination.get_count(data)),
+            ('next', pagination.get_next_link()),
+            ('previous', pagination.get_previous_link()),
+            ('started', status_count["started"]),
+            ('blocked', status_count["blocked"]),
+            ('stopped', status_count["stopped"]),
+            ('results', page)
+        ]))
 
 
 class VpsStatusUpdateView(UpdateAPIView):
